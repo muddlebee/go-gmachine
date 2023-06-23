2a82751cfff7f31a6150f6270329f2b3a7712d34
Anwesh <anweshknayak@gmail.com>
2023-06-24 02:26:43 +0530

    feat(init): thx copilot

--- gmachine.go
+++ gmachine.go
@@ -1,2 +1,20 @@
 // Package gmachine implements a simple virtual CPU, known as the G-machine.
 package gmachine
+
+type Word int
+
+// DefaultMemSize is the default size of the memory of a G-machine	.
+const DefaultMemSize = 1024
+
+// Gmachine is a virtual CPU.
+type Gmachine struct {
+	P      Word
+	Memory []Word
+}
+
+func New() *Gmachine {
+	return &Gmachine{
+		P:      0,
+		Memory: make([]Word, DefaultMemSize),
+	}
+}
